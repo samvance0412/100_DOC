4c59209a6059f074ae9e8e0bfb80366f93ba0f62
samvance0412 <37813638+samvance0412@users.noreply.github.com>
2018-04-09 18:47:33 -0400

    Rename log.md to log.sv

---
+++ log.sv
@@ -0,0 +1,14 @@
+# 100_DOC
+100 Days of Code - Log
+
+### Day 1: April 9, 2018 
+
+**Today's Progress**: Started FreeCodeCamp HTML & CSS Challenges, Second set of Data Science coursework, Joined BSIC Impact Incubator Challenge team and read a lot about the different coding languages 
+
+**Thoughts:** Basic concepts are digestible but the definitions can trip me up - difference between Class and style etc. 
+
+### Day 2: April 10, 2018 
+
+**Today's Progress**:
+
+**Thoughts:**
